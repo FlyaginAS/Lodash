7f6542695209414450631716d5715cc18c93ee92
FlyaginAS <rus.reg.36@gmail.com>
2020-09-09 19:44:31 +0300

    create 1 chunk

--- array/1chunk.js
+++ array/1chunk.js
@@ -10,5 +10,20 @@ _.chunk(['a', 'b', 'c', 'd'], 3);
 // => [['a', 'b', 'c'], ['d']]
  */
 
-const chunk = (arr, size) => {};
-console.log(chunk(["a", "b", "c", "d"], 2));
+const chunk = (arr, size) => {
+  let resultArr = [];
+  const createSubArray = (item) => {};
+  //определим количество кусков
+  const numOfChunks = Math.floor(arr.length / size);
+  console.log(numOfChunks);
+  //для каждого куска
+  for (let i = 0; i < numOfChunks; i++) {
+    let subArray = [];
+    for (let j = 0; j < size; j++) {
+      subArray.push(arr.shift());
+    }
+    resultArr[i] = subArray;
+  }
+  return (resultArr = [...resultArr, ...arr]);
+};
+console.log(chunk(['a', 'b', 'c', 'd'], 3));
