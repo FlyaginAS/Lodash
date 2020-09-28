30dc7c246a4bbeee94d72201f263f6db5ad8477e
FlyaginAS <rus.reg.36@gmail.com>
2020-09-28 11:41:56 +0300

    1countBy

---
+++ collection/1countBy.js
@@ -0,0 +1,26 @@
+/**
+ *Creates an object composed of keys generated from the results
+  of running each element of collection thru iteratee.
+   The corresponding value of each key is the number of times
+    the key was returned by iteratee
+ *
+ *
+ *
+ *
+ */
+const countBy = (arr, func) => {
+  const obj = {};
+  const copy = arr.map((item) => {
+    return func(item);
+  });
+  copy.forEach((item) => {
+    obj[`${item}`] = 0;
+  });
+  copy.forEach((item) => {
+    obj[`${item}`] += 1;
+  });
+  return obj;
+};
+
+console.log(countBy([6.1, 4.2, 6.3], Math.floor));
+// => { '4': 1, '6': 2 }
